7a1a840b3223bec9fe8383269eaa12bf59c35e20
unknown <k.moskal9901@gmail.com>
2022-11-07 22:00:44 +0300

    added scripts

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 function stateChange1() {
-    var obj = document.getElementById(`15`, `16`, `17`);
+    var obj = document.getElementById(`15`);
     obj.style.display =
-        (obj.style.display == "none" ?
-        "block" : "none");
+        (obj.style.display == "none" ? "block" : "none");
 }
 function stateChange2() {
     var obj = document.getElementById(`16`);
     obj.style.display =
-        (obj.style.display == "none" ?
-        "block" : "none");
+        (obj.style.display == "none" ? "block" : "none");
 }
 function stateChange3() {
     var obj = document.getElementById(`17`);
     obj.style.display =
-        (obj.style.display == "none" ?
-        "block" : "none");
+        (obj.style.display == "none" ? "block" : "none");
 }
