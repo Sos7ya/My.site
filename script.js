ae7c93882d827fb73c75ef2a0c148243319518d7
unknown <k.moskal9901@gmail.com>
2022-11-08 10:28:19 +0300

    added slider

--- script.js
+++ script.js
@@ -13,3 +13,32 @@ function stateChange3() {
     obj.style.display =
         (obj.style.display == "none" ? "block" : "none");
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+    
+    function nextSlide() {
+        showSlides(slideIndex += 1);
+    }
+
+    function previousSlide() {
+        showSlides(slideIndex -= 1);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+        let slides = document.getElementsByClassName("section__slide");
+        if (n > slides.length) {
+            slideIndex = 1
+        }
+        if (n < 1) {
+            slideIndex = slides.length
+        }
+        for (let slide of slides) {
+            slide.style.display = "none";
+        }
+        slides[slideIndex - 1].style.display = "block";
+    }
