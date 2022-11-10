ffafc8bc919a2d26353ecdbd04b6541bf6b5d47b
unknown <k.moskal9901@gmail.com>
2022-11-10 12:33:31 +0300

    modify function

--- script.js
+++ script.js
@@ -1,44 +1,19 @@
-function stateChange1() {
-    var obj = document.getElementById(`15`);
-    obj.style.display =
-        (obj.style.display == "none" ? "block" : "none");
-}
-function stateChange2() {
-    var obj = document.getElementById(`16`);
-    obj.style.display =
-        (obj.style.display == "none" ? "block" : "none");
-}
-function stateChange3() {
-    var obj = document.getElementById(`17`);
-    obj.style.display =
-        (obj.style.display == "none" ? "block" : "none");
-}
+document.addEventListener(`DOMContentLoaded`, () => {
+    const dropBtn = document.querySelectorAll('.dropdown__arrow');
+    const dropCont = document.querySelectorAll('.dropdown__content')
 
-let slideIndex = 1;
-showSlides(slideIndex);
-    
-    function nextSlide() {
-        showSlides(slideIndex += 1);
+    const setDisableToggle = () =>{
+        dropCont.forEach((el) =>{
+           el.classList.remove('isActive')
+        })
     }
 
-    function previousSlide() {
-        showSlides(slideIndex -= 1);
-    }
+    const handleClick = (evt) => {
+        const el = document.querySelector(`.dropdown__content_${evt.target.dataset.num}`)
+        setDisableToggle()
+        el.classList.add('isActive')
+    };
 
-    function currentSlide(n) {
-        showSlides(slideIndex = n);
-    }
+    dropBtn.forEach((el)=>el.addEventListener("click", (evt) => handleClick(evt)))
+})
 
-    function showSlides(n) {
-        let slides = document.getElementsByClassName("section__slide");
-        if (n > slides.length) {
-            slideIndex = 1
-        }
-        if (n < 1) {
-            slideIndex = slides.length
-        }
-        for (let slide of slides) {
-            slide.style.display = "none";
-        }
-        slides[slideIndex - 1].style.display = "block";
-    }
